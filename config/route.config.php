7377bf63a0e940834e3afa64dc0f355d79ff3a0f
cristian.incarnato@sondeos.com.ar <Neverwinter2.!>
2019-02-06 17:54:00 -0300

    Route back comp

--- config/route.config.php
+++ config/route.config.php
@@ -3,11 +3,31 @@
 return [
     'router' => [
         'routes' => [
-            'zfmr' => [
+            'zfmrapi' => [
                 'type' => 'Literal',
                 'mayTerminate' => false,
                 'options' => [
                     'route' => '/api',
+                ],
+                'child_routes' => [
+                    'api' => [
+                        'type' => 'Segment',
+                        'mayTerminate' => false,
+                        'options' => [
+                            'route' => '/:entityAlias[/:id]',
+                            'defaults' => [
+                                'controller' => \ZfMetal\Restful\Controller\MainController::CLASS,
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+            //Back compatibility
+            'zfmr' => [
+                'type' => 'Literal',
+                'mayTerminate' => false,
+                'options' => [
+                    'route' => '/zfmr/api',
                 ],
                 'child_routes' => [
                     'api' => [
